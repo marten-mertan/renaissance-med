6779e2c5945208aa179ce0b5950a1192d19b9ba1
Mertan <trubin-sergei@bk.ru>
2020-01-20 11:01:56 +0300

    edit images paths

--- src/scripts/vendor/markerclusterer.js
+++ src/scripts/vendor/markerclusterer.js
@@ -190,7 +190,7 @@ function MarkerClusterer(map, opt_markers, opt_options) {
  * @type {string}
  * @private
  */
-MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = '../../assets/img/m';
+MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = '../assets/img/m';
 
 
 /**
